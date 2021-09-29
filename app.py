f921cb36c38621316d00b506d52e0917a95abf0e
Machado Gamboa, Kevin <km20329@essex.ac.uk>
2021-09-29 22:52:22 +0100

    Adding basic files for app and notebook where models are trained

---
+++ app.py
@@ -0,0 +1,84 @@
+"""
+App File
+--------
+"""
+from flask import Flask, request, render_template
+import os
+import pickle
+
+
+path = os.getcwd()
+
+with open('Models/pickle_MLP_model.pkl', 'rb') as f:
+    logistic = pickle.load(f)
+
+with open('Models/pickle_MLP_model.pkl', 'rb') as f:
+    randomforest = pickle.load(f)
+
+with open('Models/pickle_MLP_model.pkl', 'rb') as f:
+    svm_model = pickle.load(f)
+
+
+def get_predictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, req_model):
+    mylist = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+    mylist = [float(i) for i in mylist]
+    vals = [mylist]
+
+    if req_model == 'Logistic':
+        # print(req_model)
+        return logistic.predict(vals)[0]
+
+    elif req_model == 'RandomForest':
+        # print(req_model)
+        return randomforest.predict(vals)[0]
+
+    elif req_model == 'SVM':
+        # print(req_model)
+        return svm_model.predict(vals)[0]
+    else:
+        return "Cannot Predict"
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def my_form_post():
+    if request.method == 'POST':
+        age = request.form['Age']
+        sex = request.form['sex']
+        cp = request.form['cp']
+        trestbps = request.form['trestbps']
+
+        chol = request.form['chol']
+        fbs = request.form['fbs']
+        restecg = request.form['restecg']
+        thalach = request.form['thalach']
+
+        exang = request.form['exang']
+        oldpeak = request.form['oldpeak']
+        slope = request.form['slope']
+        ca = request.form['ca']
+        thal = request.form['thal']
+
+        req_model = request.form['req_model']
+
+        target = get_predictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach,
+                                 exang, oldpeak, slope, ca, thal, req_model)
+
+        if target == 1:
+            sale_making = 'Potential heart disease detected .. Go see a doctor'
+        else:
+            sale_making = 'unlikely to have a heart disease .. keep exercising'
+
+        return render_template('home.html', target=target, sale_making=sale_making)
+    else:
+        return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
